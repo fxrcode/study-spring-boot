bc3e4ea871867edf5417f8b5a28923e1c96be351
fxrcode <fxrcode@gmail.com>
2021-05-04 19:30:00 -0700

    played Messaging with Redis

---
+++ spring-boot/messaging-redis/src/test/java/com/fxr/springboot/messagingredis/MessagingRedisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fxr.springboot.messagingredis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessagingRedisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
